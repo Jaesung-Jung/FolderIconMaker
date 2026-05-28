4d4602f8b096feb58c0608be141471345af9f260
Jaesung Jung <jaesung.jung1986@gmail.com>
2026-05-28 17:13:03 +0900

    build(app): scaffold tahoe folder composer
    - Add the macOS SwiftUI app target
    - Bundle the Tahoe folder base images as app resources

---
+++ TahoeFolderComposer/ContentView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Tahoe Folder Composer")
+            .frame(width: 900, height: 620)
+    }
+}
---
+++ TahoeFolderComposer/TahoeFolderComposerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct TahoeFolderComposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
