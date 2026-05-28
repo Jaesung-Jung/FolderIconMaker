8fd2929d9195746f5eedbbb30010afaab9dd7115
Jaesung Jung <jaesung.jung1986@gmail.com>
2026-05-28 18:13:00 +0900

    feat(finder): add folder icon applier
    - Apply rendered images through NSWorkspace custom icons
    - Keep Finder integration intentionally small for local use

---
+++ TahoeFolderComposer/SystemIntegration/FinderIconApplier.swift
@@ -0,0 +1,10 @@
+import AppKit
+import CoreGraphics
+import Foundation
+
+enum FinderIconApplier {
+    static func apply(_ image: CGImage, to folderURL: URL) -> Bool {
+        let nsImage = NSImage(cgImage: image, size: NSSize(width: image.width, height: image.height))
+        return NSWorkspace.shared.setIcon(nsImage, forFile: folderURL.path, options: [])
+    }
+}
